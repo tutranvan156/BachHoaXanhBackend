1bf54c12f2554f1089cb3fef3ad1399019b52159
thachpham2k <phamthach2506@gmail.com>
2022-05-08 21:36:40 +0700

    upload File Controller

--- src/main/java/ptit/example/bachhoaxanhbackend/BachHoaXanhBackendApplication.java
+++ src/main/java/ptit/example/bachhoaxanhbackend/BachHoaXanhBackendApplication.java
@@ -1,13 +1,27 @@
 package ptit.example.bachhoaxanhbackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import ptit.example.bachhoaxanhbackend.storage.StorageProperties;
+import ptit.example.bachhoaxanhbackend.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class BachHoaXanhBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BachHoaXanhBackendApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+//            storageService.deleteAll();
+            storageService.init();
+        };
     }
 
 }
